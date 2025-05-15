8d6ce577fc87a8884d7b96dd3ceffba70fac971f
bharathrams <bharathram@navadhiti.com>
2025-05-15 10:49:55 +0530

    changed th eurl

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   const [students, setStudents] = useState<Student[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/students')
+    fetch('https://mono-repo-react-node.onrender.com/api/students')
       .then(response => response.json())
       .then((data: Student[]) => setStudents(data))
       .catch(error => console.error('Error fetching students:', error));
